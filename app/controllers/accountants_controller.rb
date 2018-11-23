320616e330d8392afba3b1145b33453447749d91
Joseph Johansen <jo.johansen@hotmail.com>
2018-11-23 14:44:13 +0000

    fixed a few bugs

--- app/controllers/accountants_controller.rb
+++ app/controllers/accountants_controller.rb
@@ -2,7 +2,7 @@ class AccountantsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
-    @accountants = policy_scope(Accountant)
+    @accountants = policy_scope(Accountant).first(50)
   end
 
   def show
