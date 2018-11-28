a3580d185df655d8fab24bb8fb2f4e81521cc4d8
Carsten Schaltz <carstenschaltz@gmail.com>
2018-11-28 14:12:06 +0000

    added to show page, syling tbc

--- app/controllers/accountants_controller.rb
+++ app/controllers/accountants_controller.rb
@@ -4,16 +4,16 @@ class AccountantsController < ApplicationController
 
   def index
     @accountants = policy_scope(Accountant).paginate(page: params[:page], per_page: 10)
-    if params[:accountant]
-      to_search = params[:accountant][:services].reject(&:blank?).map(&:to_i)
-      base_sql = "SELECT id FROM \"accountants\" WHERE EXISTS (SELECT \"accountant_services\".* FROM \"accountant_services\" WHERE (accountants.id = accountant_services.accountant_id AND service_id = #{to_search.first}))"
-      to_search[1..-1].each do |id|
-        add_serv = " AND EXISTS (SELECT \"accountant_services\".* FROM \"accountant_services\" WHERE (accountants.id = accountant_services.accountant_id AND service_id = #{id}))"
-        base_sql += add_serv
-      end
-      results = Accountant.find_by_sql(base_sql)
-      @accountants = Accountant.where(id: results.map(&:id)).paginate(page: params[:page], per_page: 10)
-    end
+    # if params[:accountant]
+    #   to_search = params[:accountant][:services].reject(&:blank?).map(&:to_i)
+    #   base_sql = "SELECT id FROM \"accountants\" WHERE EXISTS (SELECT \"accountant_services\".* FROM \"accountant_services\" WHERE (accountants.id = accountant_services.accountant_id AND service_id = #{to_search.first}))"
+    #   to_search[1..-1].each do |id|
+    #     add_serv = " AND EXISTS (SELECT \"accountant_services\".* FROM \"accountant_services\" WHERE (accountants.id = accountant_services.accountant_id AND service_id = #{id}))"
+    #     base_sql += add_serv
+    #   end
+    #   results = Accountant.find_by_sql(base_sql)
+    #   @accountants = Accountant.where(id: results.map(&:id)).paginate(page: params[:page], per_page: 10)
+    # end
 
     if params[:search].present?
       @accountants = @accountants.near(params[:search], 50, order: 'distance')
