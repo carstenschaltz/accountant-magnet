fcc165c920e2a0355b301c897bdc65c521c1ca62
Joseph Johansen <jo.johansen@hotmail.com>
2018-11-28 15:16:58 +0000

    modified job

--- app/jobs/load_industries_job.rb
+++ app/jobs/load_industries_job.rb
@@ -7,6 +7,8 @@ class LoadIndustriesJob < ApplicationJob
     end
 
     Accountant.all.each do |accountant|
+      next unless accountant.industries_string
+
       industry_array = accountant.industries_string.gsub(/ \(.*\)/, "").split(", ")
       industry_array.each do |name|
         AccountantIndustry.create!(accountant: accountant, industry: Industry.find_by(name: name))
