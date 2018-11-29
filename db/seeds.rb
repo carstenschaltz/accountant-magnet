786d809231ef88d90a9d0f14156c0309e23e25be
Carsten Schaltz <carstenschaltz@gmail.com>
2018-11-29 12:19:18 +0000

    comment out seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,22 +11,22 @@
 #   Service.create! name: service
 # end
 
-u = User.create! name: "Paul", email: 'paul@abracadabra.com', password: 'password', company_size: '<1', company_name: 'Abracadabra Magicians Ltd', company_industry: 'Entertainment', company_location: 'London'
-u2 = User.create! name: "James", email: 'james@slimline.org', password: 'password', company_size: '1-10', company_name: 'Slimline Fashion inc', company_industry: 'Retail', company_location: 'Oxford'
-u3 = User.create! name: "Jennifer", email: 'jen@thedailyreporter.org', password: 'password', company_size: '10-25', company_name: 'The Daily Reporter', company_industry: 'Printing and Publishing', company_location: 'Reading'
-u4 = User.create! name: "Steven", email: 'steven.s@btinternet.net', password: 'password', company_size: '<1', company_name: 'The Fox and Hounds', company_industry: 'Catering (Pubs, Restaurants, Food and Drink)', company_location: 'Exeter'
+# u = User.create! name: "Paul", email: 'paul@abracadabra.com', password: 'password', company_size: '<1', company_name: 'Abracadabra Magicians Ltd', company_industry: 'Entertainment', company_location: 'London'
+# u2 = User.create! name: "James", email: 'james@slimline.org', password: 'password', company_size: '1-10', company_name: 'Slimline Fashion inc', company_industry: 'Retail', company_location: 'Oxford'
+# u3 = User.create! name: "Jennifer", email: 'jen@thedailyreporter.org', password: 'password', company_size: '10-25', company_name: 'The Daily Reporter', company_industry: 'Printing and Publishing', company_location: 'Reading'
+# u4 = User.create! name: "Steven", email: 'steven.s@btinternet.net', password: 'password', company_size: '<1', company_name: 'The Fox and Hounds', company_industry: 'Catering (Pubs, Restaurants, Food and Drink)', company_location: 'Exeter'
 ua = User.create! name: "Admin", email: 'admin@admin.com', password: 'Data4days', admin: true
 
-e = Enquiry.create! title: "Import/Export taxation", user: u, email: 'paul@abracadabra.com', is_local: true, description: "My company imports and distributes a lot of equipment from China, looking for some help with taxes."
-e2 = Enquiry.create! title: "General Bookkeeping", user: u2, email: 'james@slimline.org', is_local: false, description: "As my company has been growing I have been looking for someone to help keep the bookkeeping under control"
-e3 = Enquiry.create! title: "Audit", user: u3, email: 'jen@thedailyreporter.org', is_local: false, description: "Looking for an accountant to help out with audits."
-e4 = Enquiry.create! title: "Financial difficulties", email: 'steven.s@btinternet.net', user: u4, is_local: true, description: "The pub is going through some hard times, we are looking for an accountant to help with insolvency."
-e5 = Enquiry.create! title: "Looking for a specalist in tax", user: u, email: 'paul@abracadabra.com', is_local: true, description: "My company is going international and I want a tax specalist."
+# e = Enquiry.create! title: "Import/Export taxation", user: u, email: 'paul@abracadabra.com', is_local: true, description: "My company imports and distributes a lot of equipment from China, looking for some help with taxes."
+# e2 = Enquiry.create! title: "General Bookkeeping", user: u2, email: 'james@slimline.org', is_local: false, description: "As my company has been growing I have been looking for someone to help keep the bookkeeping under control"
+# e3 = Enquiry.create! title: "Audit", user: u3, email: 'jen@thedailyreporter.org', is_local: false, description: "Looking for an accountant to help out with audits."
+# e4 = Enquiry.create! title: "Financial difficulties", email: 'steven.s@btinternet.net', user: u4, is_local: true, description: "The pub is going through some hard times, we are looking for an accountant to help with insolvency."
+# e5 = Enquiry.create! title: "Looking for a specalist in tax", user: u, email: 'paul@abracadabra.com', is_local: true, description: "My company is going international and I want a tax specalist."
 
-es = EnquiryService.create! service: Service.all[3], enquiry: e
-es2 = EnquiryService.create! service: Service.all[5], enquiry: e2
-es3 = EnquiryService.create! service: Service.all[1], enquiry: e3
-es4 = EnquiryService.create! service: Service.all[2], enquiry: e4
+# es = EnquiryService.create! service: Service.all[3], enquiry: e
+# es2 = EnquiryService.create! service: Service.all[5], enquiry: e2
+# es3 = EnquiryService.create! service: Service.all[1], enquiry: e3
+# es4 = EnquiryService.create! service: Service.all[2], enquiry: e4
 
 # old accountant data
 # a = Accountant.create! name: 'Simmonds', email: 'enquiries@simmonds.org', phone_number: '020 733 9542', bio: 'Specialising in large businesses', location: "Manchester", website: 'www.accountant-directory.co.uk'
@@ -48,10 +48,10 @@ es4 = EnquiryService.create! service: Service.all[2], enquiry: e4
 # as8 = AccountantService.create! accountant: a, service: Service.all[4]
 # as8 = AccountantService.create! accountant: a, service: Service.all[6]
 
-q = Quote.create! message: "Can you solve my problems?", invite: true, successful: true, enquiry: e, accountant: Accountant.find(3)
-q2 = Quote.create! message: "Can you help me manage my books?", invite: true, successful: false, enquiry: e2, accountant: Accountant.find(30)
-q3 = Quote.create! message: "I specialize in your industry, I can help you with your audits", invite: false, successful: true, enquiry: e3, Accountant.find(300)
-q4 = Quote.create! message: "I've helped numerous pubs like yours out of the financial difficulties your having. Happy to help here too!", invite: false, successful: false, enquiry: e4, accountant: Accountant.find(3000)
-q5 = Quote.create! message: "£100/h for our tax expertise", invite: false, successful: false, enquiry: e, accountant: Accountant.find(5000)
+# q = Quote.create! message: "Can you solve my problems?", invite: true, successful: true, enquiry: e, accountant: Accountant.find(3)
+# q2 = Quote.create! message: "Can you help me manage my books?", invite: true, successful: false, enquiry: e2, accountant: Accountant.find(30)
+# q3 = Quote.create! message: "I specialize in your industry, I can help you with your audits", invite: false, successful: true, enquiry: e3, Accountant.find(300)
+# q4 = Quote.create! message: "I've helped numerous pubs like yours out of the financial difficulties your having. Happy to help here too!", invite: false, successful: false, enquiry: e4, accountant: Accountant.find(3000)
+# q5 = Quote.create! message: "£100/h for our tax expertise", invite: false, successful: false, enquiry: e, accountant: Accountant.find(5000)
 
 
